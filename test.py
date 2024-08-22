1c5908f67b887feb2e73b0525f3574d32c1e2d38
pepsijoa <hahahagunwoo@naver.com>
2024-08-22 11:33:23 +0900

    test for camera video stream

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print("hello")
+import cv2
+import cv2.aruco as aruco
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+piCam2.preview_configuration.main.size=(1280, 720)
+piCam2.preview_configuration.main.format ="RGB888"
+piCam2.preview_configuration.align()
+piCam2.configure("preview")
+piCam.start()
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters_create()
+
+# 비디오 캡처 또는 이미지 읽기
+
+while True:
+    frame = piCam2.capture_array()
+    cv2.imshow("piCam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cv2.destroyAllWindows()
